2a225cd17f0eaec0bcc88ef8ce7da7ee74e61333
Kerstiens <nkerstiens1@una.edu>
2022-11-15 13:44:50 -0600

    added some extra styles into home page

--- public/main.js
+++ public/main.js
@@ -19,3 +19,11 @@ $(".delete").click(function(){
     })
   
   })
+
+  $('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
